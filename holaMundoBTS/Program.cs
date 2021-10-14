1e5eff3d7258483b874cb326c7985545a008ea8b
Vic1506 <vieich1506@gmail.com>
2021-10-13 18:56:42 -0700

    first commit

---
+++ holaMundoBTS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace holaMundoBTS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! HAPPY BIRTHDAY JIMIN");
+        }
+    }
+}
